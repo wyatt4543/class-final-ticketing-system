5aaeb2599208dc889b4937be78b447dea2872615
wyatt4543 <w4543w@gmail.com>
2026-05-13 03:17:49 -0500

    remove next from the user model

--- models/User.js
+++ models/User.js
@@ -9,10 +9,10 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+    if (!this.isModified('password')) return;
+
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
